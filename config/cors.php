870cb7750017ff34be4ea1e488878bac4a14a4d1
giuseppecasaburi <enzobiondi0000@libero.it>
2025-05-30 22:31:52 +0200

    feat: add CORS configuration and render settings for Laravel backend

---
+++ config/cors.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://mooviemood.netlify.app', // Sostituisci con il tuo dominio frontend
+        'http://localhost:5173' // Per test locali con Vite
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
